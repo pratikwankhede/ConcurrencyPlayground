01124c182787f2b0bc0d88e7c53c6ba47219809d
pratikwankhede <pratikwankhede007@gmail.com>
2025-01-28 22:48:16 +0530

    2 threads

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         UserRepository ur = new UserRepository();
         ur.add(new User(1,"A","ash"));
@@ -12,9 +12,17 @@ public class Main {
 
             for(int i=0;i<200;i++) ur.increment(1);
         });
-        thread1.start();
+        Thread thread2 = new Thread(() -> {
 
-        for(int i=0;i<200;i++) ur.increment(1);
+            for(int i=0;i<200;i++) ur.increment(1);
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+
+
         System.out.println(ur.get(1).param);
 
 
